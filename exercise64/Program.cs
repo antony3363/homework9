972c2cd6541c31dac2661a2d02c0c0bbaac9de82
Anton <anton_artemjev06@mail.ru>
2022-09-07 18:32:50 +0700

    ДЗ-9

---
+++ exercise64/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 64: Задайте значения M и N. Напишите программу, которая найдет сумму чисел от M до N.
+
+//M = 1; N = 5. -> 1, 2, 3, 4, 5
+
+//M = 4; N = 8. -> 4, 6, 7, 8
+
+System.Console.WriteLine("Введите M");
+int M = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите N");
+int N = int.Parse(Console.ReadLine()!);
+
+System.Console.Write("->");
+
+for (int i = M; i <= N; i++)
+{
+    System.Console.Write(" " + i + ",");
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ exercise69/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+//A = 3; B = 5 -> 243 (3⁵)
+
+//A = 2; B = 3 -> 8
+
+
+System.Console.WriteLine("Введите A");
+int A = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите B");
+int B = int.Parse(Console.ReadLine()!);
+
+int C = A;
+
+for (int i = 1; i < B; i++)
+{
+    C*=A;
+}
+System.Console.WriteLine(C);
+
+
+
+
+
+
+
+
